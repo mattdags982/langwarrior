092d4e7a80f9998b0e71426943c991a192ecbe3e
mattdags982 <mattd@wizehire.com>
2024-08-04 17:27:31 -0400

    use modules in story select view

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -1,73 +1,26 @@
-import Image from "next/image";
+import { getAllModules } from "@/app/api/modules";
 
-const people = [
-  {
-    name: "Leslie Alexander",
-    email: "leslie.alexander@example.com",
-    role: "Co-Founder / CEO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    name: "Michael Foster",
-    email: "michael.foster@example.com",
-    role: "Co-Founder / CTO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    name: "Dries Vincent",
-    email: "dries.vincent@example.com",
-    role: "Manager, Business Relations",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    name: "Lindsay Walton",
-    email: "lindsay.walton@example.com",
-    role: "Front-end Developer",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    name: "Courtney Henry",
-    email: "courtney.henry@example.com",
-    role: "Designer",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    name: "Tom Cook",
-    email: "tom.cook@example.com",
-    role: "Director of Product",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  // More people...
-];
+export const fetchCache = "default-no-store";
 
-export default function Example() {
+export default async function Example() {
+  const modules = await getAllModules();
+  console.log(modules[0].chapters);
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-      {people.map((person) => (
+      {modules.map((module) => (
         <div
-          key={person.email}
+          key={module.id}
           className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
         >
-          <div className="flex-shrink-0">
-            <Image
-              alt=""
-              src={person.imageUrl}
-              width={32}
-              height={32}
-              className="h-10 w-10 rounded-full"
-            />
-          </div>
           <div className="min-w-0 flex-1">
             <a href="#" className="focus:outline-none">
               <span aria-hidden="true" className="absolute inset-0" />
-              <p className="text-sm font-medium text-gray-900">{person.name}</p>
-              <p className="truncate text-sm text-gray-500">{person.role}</p>
+              <p className="text-sm font-medium text-gray-900">
+                {module.title}
+              </p>
+              <p className="truncate text-sm text-gray-500">
+                {module.description}
+              </p>
             </a>
           </div>
         </div>
