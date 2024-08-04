4de982e039c91282e5f71ac5fa4e14ab694bb9b1
mattdags982 <mattd@wizehire.com>
2024-08-04 18:36:00 -0400

    rough version of audio player

--- src/app/modules/[module_id]/[chapter_id]/page.tsx
+++ src/app/modules/[module_id]/[chapter_id]/page.tsx
@@ -1,4 +1,5 @@
 import { getChapterById } from "@/app/api/modules";
+import AudioPlayer from "@/app/components/AudioPlayer";
 
 interface Props {
   params: {
@@ -12,31 +13,34 @@ export default async function Example({
 }: Props) {
   const chapter = await getChapterById(chapter_id, "es");
   return (
-    <div className="py-10">
-      <header>
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
-            {chapter.title}
-          </h1>
-          <p className="mt-2 text-lg text-gray-600">{chapter.description}</p>
-        </div>
-      </header>
-      <main>
-        <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-          <ul role="list" className="divide-y divide-gray-200">
-            {chapter.blurbs.map((blurb) => (
-              <li key={blurb.id} className="py-4">
-                <p className="text-sm font-medium text-gray-900">
-                  {blurb.characterName}
-                </p>
-                <p className="truncate text-sm text-gray-500">
-                  {blurb.contentEnglish}
-                </p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </main>
-    </div>
+    <>
+      <div className="py-10 mb-14">
+        <header>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+              {chapter.title}
+            </h1>
+            <p className="mt-2 text-lg text-gray-600">{chapter.description}</p>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+            <ul role="list" className="divide-y divide-gray-200">
+              {chapter.blurbs.map((blurb) => (
+                <li key={blurb.id} className="py-4">
+                  <p className="text-sm font-medium text-gray-900">
+                    {blurb.characterName}
+                  </p>
+                  <p className="truncate text-sm text-gray-500">
+                    {blurb.contentEnglish}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </main>
+      </div>
+      <AudioPlayer audioSrc="/audio/next.mp3" />
+    </>
   );
 }
