6da871dfdd8e903e0a8f36fa6356579744f24f41
Matt D'Agostino <mattdags982@gmail.com>
2024-12-06 17:24:09 -0500

    chapters instead of story name

--- src/app/modules/[module_id]/page.tsx
+++ src/app/modules/[module_id]/page.tsx
@@ -13,7 +13,7 @@ export default async function Example({ params: { module_id } }: Props) {
       <header>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
-            Story Name
+            Chapters
           </h1>
         </div>
       </header>
