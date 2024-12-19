7d360e9faf68c0f557296c278343bf31679a5094
Matt D'Agostino <mattdags982@gmail.com>
2024-12-19 15:35:49 -0500

    extend default runtime caching

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,12 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  swcMinify: true,
   disable: false,
   workboxOptions: {
     disableDevLogs: true,
   },
+  extendDefaultRuntimeCaching: true,
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/res\.cloudinary\.com\/.*\/audio\/.*\.mp3$/,
